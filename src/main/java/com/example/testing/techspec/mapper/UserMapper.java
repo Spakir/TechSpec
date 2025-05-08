6a0a73a2b41b04826553c9e3ba8724ea7792dc72
Spakir <danntyl@mail.ru>
2025-05-08 06:12:10 +0300

    mapper

--- src/main/java/com/example/testing/techspec/mapper/UserMapper.java
+++ src/main/java/com/example/testing/techspec/mapper/UserMapper.java
@@ -2,7 +2,10 @@ package com.example.testing.techspec.mapper;
 
 import com.example.testing.techspec.dto.UserDto;
 import com.example.testing.techspec.entities.User;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring", uses = SubscribeMapper.class)
 public interface UserMapper {
@@ -10,4 +13,7 @@ public interface UserMapper {
     UserDto toDto(User user);
 
     User toEntity(UserDto dto);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateUserFromDto(UserDto dto, @MappingTarget User user);
 }
